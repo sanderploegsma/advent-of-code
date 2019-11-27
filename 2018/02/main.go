22ded684643ae9d536dfa288d3574c4f54add9a0
Sander Ploegsma <sander@xebia.studio>
2019-11-27 09:05:22 +0100

    Practice: AOC2018 day 2 (part 2)

--- 2018/02/main.go
+++ 2018/02/main.go
@@ -12,7 +12,15 @@ func main() {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	lines := strings.Split(string(input), "\n")
+
 	log.Printf("checksum: %d\n", Checksum(lines))
+
+	match, ok := MatchBoxIDs(lines)
+	if ok {
+		log.Printf("match: %s\n", match)
+	} else {
+		log.Println("no match")
+	}
 }
 
 // checksum calculates the checksum over the given list of `boxIds`.
@@ -49,4 +57,53 @@ func hasNOfAnyLetter(boxId string, n int) bool {
 	}
 
 	return false
+}
+
+// MatchBoxIDs looks for a pair of box IDs in the given list that differ by exactly one character at the same position. It returns the boxID with that one character removed, if found.
+func MatchBoxIDs(boxIDs []string) (string, bool) {
+	if len(boxIDs) == 0 {
+		return "", false
+	}
+
+	length := len(boxIDs[0])
+	for pos := 0; pos < length; pos++ {
+		altered := make([]string, len(boxIDs))
+
+		for i, boxID := range boxIDs {
+			altered[i] = removeCharacterAt(boxID, pos)
+		}
+
+		if found, ok := findDuplicate(altered); ok {
+			return found, true
+		}
+	}
+
+	return "", false
+}
+
+// findDuplicate looks for a pair of strings in the given list that are equal. It returns the duplicate string if found.
+func findDuplicate(input []string) (string, bool) {
+	var visited = make(map[string]bool)
+	for _, item := range input {
+		if visited[item] {
+			return item, true
+		}
+		visited[item] = true
+	}
+	return "", false
+}
+
+// removeCharacterAt takes an input string and removes the character at the given position. The result of the operation is returned
+func removeCharacterAt(input string, position int) string {
+	length := len(input)
+
+	if position == 0 {
+		return input[1:]
+	}
+
+	if position == len(input)-1 {
+		return input[0 : length-1]
+	}
+
+	return input[0:position] + input[position+1:]
 }
--- 2018/02/main_test.go
+++ 2018/02/main_test.go
@@ -20,3 +20,26 @@ func TestExample(t *testing.T) {
 		t.Fatalf("expected %d but got %d", expected, actual)
 	}
 }
+
+func TestExampleTwo(t *testing.T) {
+	input := []string{
+		"abcde",
+		"fghij",
+		"klmno",
+		"pqrst",
+		"fguij",
+		"axcye",
+		"wvxyz",
+	}
+
+	expected := "fgij"
+	actual, ok := MatchBoxIDs(input)
+
+	if !ok {
+		t.Fatal("expected a match but got none")
+	}
+
+	if actual != expected {
+		t.Fatalf("expected '%s' but got '%s'", expected, actual)
+	}
+}
