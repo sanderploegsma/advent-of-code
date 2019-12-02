79bc11872fbb8c4b549ced75058269d970089002
Sander Ploegsma <sander@xebia.studio>
2019-12-02 08:41:54 +0100

    AOC2019 day 1 (part 2)

--- 2019/01/main.go
+++ 2019/01/main.go
@@ -13,10 +13,11 @@ func main() {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	lines := strings.Split(string(input), "\n")
-	log.Printf("fuel requirements: %d", CalculateRequiredFuel(lines))
+	log.Printf("[PART ONE] fuel requirements: %d", PartOne(lines))
+	log.Printf("[PART TWO] fuel requirements: %d", PartTwo(lines))
 }
 
-func CalculateRequiredFuel(input []string) int {
+func PartOne(input []string) int {
 	fuel := 0
 	for _, module := range input {
 		mass, _ := strconv.Atoi(module)
@@ -24,4 +25,24 @@ func CalculateRequiredFuel(input []string) int {
 	}
 
 	return fuel
+}
+
+func PartTwo(input []string) int {
+	fuel := 0
+	for _, module := range input {
+		mass, _ := strconv.Atoi(module)
+		fuel += CalculateFuel(mass)
+	}
+
+	return fuel
+}
+
+// CalculateFuel calculates the total fuel required for the given mass recursively until it reaches zero.
+func CalculateFuel(mass int) int {
+	fuel := mass/3 - 2
+	if fuel < 0 {
+		return 0
+	}
+
+	return fuel + CalculateFuel(fuel)
 }
--- 2019/01/main_test.go
+++ 2019/01/main_test.go
@@ -18,7 +18,27 @@ func TestPartOne(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("mass of %s requires %d fuel", test.mass, test.fuel), func(t *testing.T) {
-			actual := CalculateRequiredFuel([]string{test.mass})
+			actual := PartOne([]string{test.mass})
+			if actual != test.fuel {
+				t.Errorf("expected %d but got %d", test.fuel, actual)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	tests := []struct {
+		mass string
+		fuel int
+	}{
+		{"14", 2},
+		{"1969", 966},
+		{"100756", 50346},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("mass of %s requires %d fuel", test.mass, test.fuel), func(t *testing.T) {
+			actual := PartTwo([]string{test.mass})
 			if actual != test.fuel {
 				t.Errorf("expected %d but got %d", test.fuel, actual)
 			}
