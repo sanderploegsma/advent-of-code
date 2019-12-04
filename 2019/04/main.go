6c45855be6349f90dc447451e6fc69b86253767f
Sander Ploegsma <sander@xebia.studio>
2019-12-04 07:16:00 +0100

    AOC2019 day 4

---
+++ 2019/04/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	one, two := CheckPasswords(254032, 789860)
+	end := time.Since(start)
+	fmt.Printf("[PART ONE] passwords that meet criteria: %d\n", one)
+	fmt.Printf("[PART TWO] passwords that meet criteria: %d\n", two)
+	fmt.Printf("took %s\n", end)
+}
+
+// CheckPasswords checks for each password in the given range of passwords whether they meet both sets of criteria.
+// For both criteria, it returns the number of passwords that meet them.
+func CheckPasswords(start int, end int) (a int, b int) {
+	for i := start; i < end; i++ {
+		if PasswordMeetsCriteria(i) {
+			a++
+		}
+
+		if PasswordMeetsCriteria2(i) {
+			b++
+		}
+	}
+	return a, b
+}
+
+// PasswordMeetsCriteria checks the given password for the following criteria:
+// - Must have exactly 6 digits
+// - Must have two adjacent digits that are the same
+// - Going from left to right, the digits never decrease
+func PasswordMeetsCriteria(password int) bool {
+	str := strconv.Itoa(password)
+
+	if len(str) != 6 {
+		return false
+	}
+
+	prev := 0
+	double := false
+	for i := range str {
+		cur, _ := strconv.Atoi(str[i : i+1])
+		if cur < prev {
+			return false
+		}
+		if cur == prev {
+			double = true
+		}
+		prev = cur
+	}
+
+	return double
+}
+
+// PasswordMeetsCriteria2 checks the given password for the following criteria:
+// - Must have exactly 6 digits
+// - Must have a group of exactly two adjacent digits that are the same
+// - Going from left to right, the digits never decrease
+func PasswordMeetsCriteria2(password int) bool {
+	str := strconv.Itoa(password)
+
+	if len(str) != 6 {
+		return false
+	}
+
+	prev := 0
+	repeat := 0
+	double := false
+	for i := range str {
+		cur, _ := strconv.Atoi(str[i : i+1])
+		if cur < prev {
+			return false
+		}
+
+		if cur > prev {
+			if repeat == 2 {
+				double = true
+			}
+			repeat = 1
+		}
+
+		if cur == prev {
+			repeat++
+		}
+
+		if i == len(str)-1 && (repeat == 2 || double) {
+			return true
+		}
+
+		prev = cur
+	}
+
+	return double
+}
---
+++ 2019/04/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartOne(t *testing.T) {
+	assert.True(t, PasswordMeetsCriteria(111111))
+	assert.False(t, PasswordMeetsCriteria(223450))
+	assert.False(t, PasswordMeetsCriteria(123789))
+}
+
+func TestPartTwo(t *testing.T) {
+	assert.True(t, PasswordMeetsCriteria2(112233), "%d should meet criteria", 112233)
+	assert.False(t, PasswordMeetsCriteria2(123444), "%d should not meet criteria", 123444)
+	assert.True(t, PasswordMeetsCriteria2(111122), "%d should meet criteria", 111122)
+}
