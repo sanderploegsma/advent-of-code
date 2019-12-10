bc509fe66a04da648922b61d094e1da2102ff872
Sander Ploegsma <sander@xebia.studio>
2019-12-10 10:40:00 +0100

    AOC2019 day 10

---
+++ 2019/10/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/sanderploegsma/advent-of-code/2019/reader"
+)
+
+func main() {
+	input, _ := reader.ReadFile("input.txt")
+	grid := ParseGrid(input)
+	start := time.Now()
+	p, num := PartOne(grid)
+	fmt.Printf("[PART ONE] position (%d, %d) can detect %d asteroids (took %s)\n", p.x, p.y, num, time.Since(start))
+
+	start = time.Now()
+	destroyed := PartTwo(grid, p)
+	fmt.Printf("[PART TWO] 200th destroyed asteroid: (%d, %d) (took %s)\n", destroyed[199].x, destroyed[199].y, time.Since(start))
+}
+
+type point struct{ x, y int }
+
+func (p *point) DistanceTo(o point) float64 {
+	return math.Sqrt(math.Pow(float64(p.x-o.x), 2) + math.Pow(float64(p.y-o.y), 2))
+}
+
+func (p *point) AngleTo(o point) float64 {
+	return math.Atan2(float64(o.x-p.x), float64(o.y-p.y))
+}
+
+func ParseGrid(input string) (asteroids []point) {
+	rows := strings.Split(input, "\n")
+
+	for y, row := range rows {
+		for x := 0; x < len(rows[0]); x++ {
+			if string(row[x]) == "#" {
+				asteroids = append(asteroids, point{x, y})
+			}
+		}
+	}
+
+	return asteroids
+}
+
+func PartOne(asteroids []point) (p point, num int) {
+	for _, a := range asteroids {
+		slopes := make([]float64, 0)
+		for _, b := range asteroids {
+			if a.x == b.x && a.y == b.y {
+				continue
+			}
+
+			d := a.AngleTo(b)
+			exists := false
+			for _, s := range slopes {
+				if d == s {
+					exists = true
+				}
+			}
+
+			if !exists {
+				slopes = append(slopes, d)
+			}
+		}
+
+		if len(slopes) > num {
+			num = len(slopes)
+			p = a
+		}
+	}
+
+	return p, num
+}
+
+func PartTwo(asteroids []point, origin point) (destroyed []point) {
+	targets := make(map[float64][]point)
+	for _, a := range asteroids {
+		if a.x == origin.x && a.y == origin.y {
+			continue
+		}
+
+		// Calculate the angle wrt origin, offsetting by 45 degrees so that directly upwards counts as 0. Also, since radians go counter-clockwise, multiply by -1
+		angle := (origin.AngleTo(a) - 0.5*math.Pi) * -1
+		if _, ok := targets[angle]; !ok {
+			targets[angle] = make([]point, 0)
+		}
+
+		targets[angle] = append(targets[angle], a)
+		// Store the targets in order ascending from closest to origin
+		sort.Slice(targets[angle], func(i, j int) bool {
+			return origin.DistanceTo(targets[angle][i]) < origin.DistanceTo(targets[angle][j])
+		})
+	}
+
+	// Order the angles ascending
+	order := make([]float64, 0)
+	for d := range targets {
+		order = append(order, d)
+	}
+	sort.Float64s(order)
+
+	// Kill 'em all
+	i := 0
+	for len(destroyed) < len(asteroids)-1 {
+		d := order[i%len(order)]
+		if len(targets[d]) > 0 {
+			destroyed = append(destroyed, targets[d][0])
+			targets[d] = targets[d][1:]
+		}
+		i++
+	}
+
+	return destroyed
+}
---
+++ 2019/10/main_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartOne(t *testing.T) {
+	input := `
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####
+	`
+
+	grid := ParseGrid(strings.TrimSpace(input))
+	p, num := PartOne(grid)
+
+	assert.Equal(t, 5, p.x)
+	assert.Equal(t, 8, p.y)
+	assert.Equal(t, 33, num)
+}
+
+func TestPartTwo(t *testing.T) {
+	input := `
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+`
+
+	asteroids := ParseGrid(strings.TrimSpace(input))
+	destroyed := PartTwo(asteroids, point{11, 13})
+
+	assert.Equal(t, 11, destroyed[0].x)
+	assert.Equal(t, 12, destroyed[0].y)
+
+	assert.Equal(t, 12, destroyed[1].x)
+	assert.Equal(t, 1, destroyed[1].y)
+
+	assert.Equal(t, 12, destroyed[2].x)
+	assert.Equal(t, 2, destroyed[2].y)
+
+	assert.Equal(t, 12, destroyed[9].x)
+	assert.Equal(t, 8, destroyed[9].y)
+
+	assert.Equal(t, 16, destroyed[19].x)
+	assert.Equal(t, 0, destroyed[19].y)
+
+	assert.Equal(t, 16, destroyed[49].x)
+	assert.Equal(t, 9, destroyed[49].y)
+
+	assert.Equal(t, 10, destroyed[99].x)
+	assert.Equal(t, 16, destroyed[99].y)
+
+	assert.Equal(t, 9, destroyed[198].x)
+	assert.Equal(t, 6, destroyed[198].y)
+
+	assert.Equal(t, 8, destroyed[199].x)
+	assert.Equal(t, 2, destroyed[199].y)
+}
