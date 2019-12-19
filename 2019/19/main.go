82923e15e319b68ff7cd4db89906a061d8d98fc2
Sander Ploegsma <sander@xebia.studio>
2019-12-19 20:57:32 +0100

    AOC2019 day 19 (part 1)

---
+++ 2019/19/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sanderploegsma/advent-of-code/2019/utils"
+)
+
+func main() {
+	instructions, err := utils.ReadIntCode("input.txt")
+	if err != nil {
+		fmt.Printf("failed to read input: %v\n", err)
+		os.Exit(1)
+	}
+
+	sum := 0
+	for x := 0; x < 50; x++ {
+		for y := 0; y < 50; y++ {
+			in := make(chan int)
+			out := make(chan int)
+
+			go utils.RunIntCode(in, out, instructions)
+			in <- x
+			in <- y
+			sum += <-out
+		}
+	}
+
+	fmt.Println(sum)
+}
