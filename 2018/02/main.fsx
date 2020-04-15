4209d312b503ee3a3c8063ecf89f4b1ffd295061
Sander Ploegsma <sander@xebia.studio>
2020-04-16 00:48:00 +0200

    2018-02 in F# (part 2)

--- 2018/02/main.fsx
+++ 2018/02/main.fsx
@@ -1,23 +1,42 @@
 open System.IO
 
-let has n (x: string) =
-    x.ToCharArray()
-    |> Array.countBy id
-    |> Array.exists (fun (_, c) -> c = n)
-
-let countIf pred = List.filter pred >> List.length
-
 /// <summary>
 /// Finds the checksum of the given box IDs by calculating
 /// the number of IDs that have 2 of the same letter
 /// times the number of IDs that have 3 of the same letter.
 /// </summary>
 let partOne ids =
-    let has2 = has 2
-    let has3 = has 3
+    let countIf pred = List.filter pred >> List.length
 
-    (ids |> countIf has2) * (ids |> countIf has3)
+    let has n (x: string) =
+        x.ToCharArray()
+        |> Array.countBy id
+        |> Array.exists (fun (_, c) -> c = n)
+
+    (ids |> countIf (has 2)) * (ids |> countIf (has 3))
+
+
+/// <summary>
+/// Finds the two box IDs that vary by only one character at the same position.
+/// </summary>
+/// <returns>The characters both box IDs have in common</returns>
+let partTwo ids =
+    let removeAt i = List.map (fun (x: string) -> x.Remove(i) + x.Substring(i + 1))
+
+    let rec findDuplicate i =
+        let duplicate =
+            ids
+            |> removeAt i
+            |> List.countBy id
+            |> List.tryFind (fun (_, c) -> c = 2)
+
+        match duplicate with
+        | Some(x, _) -> x
+        | None -> findDuplicate (i + 1)
+
+    findDuplicate 0
 
 let input = File.ReadLines("input.txt") |> Seq.toList
 
 printfn "Part one: %d" <| partOne input
+printfn "Part two: %s" <| partTwo input
