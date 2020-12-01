a67c712eccb913917f7bbd2b504b1ae702b87a81
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-01 10:10:15 +0100

    AOC2020 Day 1

--- 2020/Day01.Test/Tests.fs
+++ 2020/Day01.Test/Tests.fs
@@ -3,6 +3,12 @@ module Day01.Test
 open Day01
 open Xunit
 
+let example = [1721; 979; 366; 299; 675; 1456]
+
 [<Fact>]
 let ``Part One`` () =
-    Assert.Equal("Hello, World!", (partOne "World"))
+    Assert.Equal(514579, (partOne example))
+    
+[<Fact>]
+let ``Part Two`` () =
+    Assert.Equal(241861950, (partTwo example))
--- 2020/Day01/Program.fs
+++ 2020/Day01/Program.fs
@@ -2,10 +2,25 @@
 
 open System.IO
 
-let partOne = sprintf "Hello, %s!"
+// https://stackoverflow.com/a/1231711
+let rec comb n l = 
+    match n, l with
+    | 0, _ -> [[]]
+    | _, [] -> []
+    | k, (x::xs) -> List.map ((@) [x]) (comb (k-1) xs) @ comb k xs
+    
+let find n l =
+    comb n l
+    |> List.filter (fun l -> List.sum l = 2020)
+    |> List.map (fun l -> List.fold (*) 1 l)
+    |> List.head
+
+let partOne = find 2
+let partTwo = find 3
 
 [<EntryPoint>]
 let main argv =
-    let input = File.ReadAllText("input.txt")
-    partOne input |> printfn "Part one: %s"
+    let input = File.ReadAllLines("input.txt") |> Seq.map int |> Seq.toList 
+    partOne input |> printfn "Part one: %d"
+    partTwo input |> printfn "Part two: %d"
     0 // return an integer exit code
