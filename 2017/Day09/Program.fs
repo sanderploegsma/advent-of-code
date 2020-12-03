11dde0b3592e7f46fd6b57596a5571095f0e37cb
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-03 16:10:58 +0100

    AOC2017 day 9 - refactor

--- 2017/Day09/Program.fs
+++ 2017/Day09/Program.fs
@@ -2,36 +2,38 @@
 
 open System.IO
 
-let traverse (input: string) =
-    let mutable level = 0
-    let mutable isGarbage = false
-    let mutable isCanceled = false
-    let mutable score = 0
-    let mutable garbage = 0
+type State = {
+    Level: int
+    Score: int
+    Garbage: int
+    IsGarbage: bool
+    IsCanceled: bool
+}
 
-    for i in 0 .. input.Length - 1 do
-        if isGarbage && isCanceled then
-            isCanceled <- false
-        else if isGarbage && input.[i] = '!' then
-            isCanceled <- true
-        else if isGarbage && input.[i] = '>' then
-            isGarbage <- false
-        else if isGarbage then
-            garbage <- garbage + 1
-        else if input.[i] = '<' then
-            isGarbage <- true
-        else if not isGarbage && input.[i] = '{' then
-            level <- level + 1
-        else if not isGarbage && input.[i] = '}' then
-            score <- score + level
-            level <- level - 1
+let initialState = {
+    Level = 0
+    Score = 0
+    Garbage = 0
+    IsGarbage = false
+    IsCanceled = false
+}
 
-    (score, garbage)
+let rec traverse (input: char list) state =
+    match input with
+    | [] -> state
+    | _ :: tail when state.IsGarbage && state.IsCanceled -> traverse tail { state with IsCanceled = false }
+    | '!' :: tail when state.IsGarbage -> traverse tail { state with IsCanceled = true }
+    | '>' :: tail when state.IsGarbage -> traverse tail { state with IsGarbage = false }
+    | _ :: tail when state.IsGarbage -> traverse tail { state with Garbage = state.Garbage + 1 }
+    | '<' :: tail -> traverse tail { state with IsGarbage = true }
+    | '{' :: tail -> traverse tail { state with Level = state.Level + 1 }
+    | '}' :: tail -> traverse tail { state with Score = state.Score + state.Level; Level = state.Level - 1 }
+    | _ :: tail -> traverse tail state
 
 [<EntryPoint>]
 let main argv =
-    let input = File.ReadAllText("input.txt")
-    let (score, garbage) = traverse input
-    printfn "Part one: %d" score
-    printfn "Part two: %d" garbage
+    let input = File.ReadAllText("input.txt") |> Seq.toList
+    let state = traverse input initialState
+    printfn "Part one: %d" state.Score
+    printfn "Part two: %d" state.Garbage
     0
