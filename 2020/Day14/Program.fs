d070dfdfc6f5f33fcd0483230abaa0788f6a63f8
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-14 10:39:14 +0100

    Refactor 2020 day 14

--- 2020/Day14/Program.fs
+++ 2020/Day14/Program.fs
@@ -14,55 +14,76 @@ let parse line =
     | Regex @"mask = ([X01]{36})" [mask] -> Mask mask
     | Regex @"mem\[(\d+)\] = (\d+)" [address; value] -> Assignment (int64 address, int64 value)
 
-let partOne input =
-    let mutable memory = Map.empty
-    let mutable mask = ""
+let mask1 (m, v) = if m = 'X' then v else m
+let mask2 (m, v) = if m = '0' then v else m
 
-    let calculateValue (value: int64) =
+let applyMask fn mask value =
+    value
+    |> Seq.zip mask
+    |> Seq.map fn
+    |> Seq.fold (sprintf "%s%c") ""
+
+/// Generate all possible values with the given floating bit positions.
+/// Example: floating bits 2, 5 and 7 would result in
+/// 00000000, 00000001, 00000100, 000000101, 00100000, 00100001, etc
+let generateValues floatingBits =
+    let length = Seq.length floatingBits
+
+    let fill values =
+        Seq.replicate 36 '0'
+        |> Seq.indexed
+        |> Seq.mapFold (fun b (i, v) -> if Seq.contains i floatingBits then (Seq.head b, Seq.skip 1 b) else (v, b)) values
+        |> fst
+
+    seq { 0 .. (pown 2 length) - 1 }
+    |> Seq.map (fun i -> Convert.ToString(i, 2).PadLeft(length, '0'))
+    |> Seq.map fill
+
+type State = Map<int64, int64> * string
+
+let apply1 (memory, mask) instruction =
+    let maskFn (value: int64) = 
         Convert.ToString(value, 2).PadLeft(36, '0')
-        |> Seq.zip mask
-        |> Seq.map (fun (m, v) -> if m = 'X' then v else m)
-        |> Seq.fold (sprintf "%s%c") ""
+        |> applyMask mask1 mask
         |> fun s -> Convert.ToInt64(s, 2)
 
-    for i in 0 .. List.length input - 1 do
-        match input.[i] with
-        | Mask m -> mask <- m
-        | Assignment (address, value) -> memory <- memory.Add(address, calculateValue value)
+    let assign address value =
+        memory |> Map.add address (maskFn value)
 
-    Map.toSeq memory
-    |> Seq.sumBy snd
+    match instruction with
+    | Mask m -> memory, m
+    | Assignment (address, value) -> (assign address value), mask
 
-let partTwo input =
-    let mutable memory = Map.empty
-    let mutable mask = ""
+let apply2 (memory, mask) instruction =
+    let maskFn (address: int64) =
+        let addressMask = 
+            Convert.ToString(address, 2).PadLeft(36, '0') 
+            |> applyMask mask2 mask
+        
+        let floatingBits = 
+            addressMask 
+            |> Seq.indexed 
+            |> Seq.filter (fun (_, c) -> c = 'X') 
+            |> Seq.map fst 
+            |> Seq.toList
 
-    let calculateAddresses (address: int64) =
-        let address' = 
-            Convert.ToString(address, 2).PadLeft(36, '0')
-            |> Seq.zip mask
-            |> Seq.map (fun (m, v) -> if m = '0' then v else m)
-            |> Seq.fold (sprintf "%s%c") ""
+        generateValues floatingBits
+        |> Seq.map (applyMask mask1 addressMask)
+        |> Seq.map (fun s -> Convert.ToInt64(s, 2))
 
-        let floating = address' |> Seq.filter (fun c -> c = 'X') |> Seq.length
-        seq {
-            for i in 0 .. (pown 2 floating) - 1 do
-                let bits = Convert.ToString(i, 2).PadLeft(floating, '0')
-                address'
-                |> Seq.mapFold (fun (b: string) c -> if c = 'X' then (Seq.head b, b.Substring(1)) else (c, b)) bits
-                |> fst
-                |> Seq.fold (sprintf "%s%c") ""
-                |> fun s -> Convert.ToInt64(s, 2)
-        }
-    
-    for i in 0 .. List.length input - 1 do
-        match input.[i] with
-        | Mask m -> mask <- m
-        | Assignment (address, value) ->
-            calculateAddresses address
-            |> Seq.iter (fun address' -> memory <- memory.Add(address', value))
+    let assign address value =
+        maskFn address
+        |> Seq.fold (fun mem addr -> Map.add addr value mem) memory
 
-    Map.toSeq memory
+    match instruction with
+    | Mask m -> memory, m
+    | Assignment (address, value) -> (assign address value), mask
+
+let run (apply: State -> Instruction -> State) input =
+    input
+    |> Seq.fold apply (Map.empty, "")
+    |> fst
+    |> Map.toSeq
     |> Seq.sumBy snd
 
 [<EntryPoint>]
@@ -72,6 +93,6 @@ let main argv =
         |> Seq.map parse
         |> Seq.toList
         
-    partOne input |> printfn "Part one: %d"
-    partTwo input |> printfn "Part two: %d"
+    input |> run apply1 |> printfn "Part one: %d"
+    input |> run apply2 |> printfn "Part two: %d"
     0
