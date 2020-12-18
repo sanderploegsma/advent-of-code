dc8fd9b7c1b53c4b2c53080c1c655b7058870dcd
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-18 11:58:14 +0100

    2020 day 18 part 2

--- 2020/Day18/Program.fs
+++ 2020/Day18/Program.fs
@@ -2,52 +2,56 @@
 
 open System.IO
 open System.Text.RegularExpressions
+open System.Collections.Generic
 
-let solveEq (eq: string) =
-    let items = eq.Split(" ")
-    let mutable value = int64 items.[0]
-    let mutable op = (*)
+let solveEq (precedence: Map<string, int>) (eq: string) =
+    // Source: https://en.wikipedia.org/wiki/Operator-precedence_parser
+    let rec solveRec value (queue: Queue<string>) min =
+        let mutable next = ""
+        let mutable lhs = value
+        while queue.TryPeek(&next) && precedence.[next] >= min do
+            let op = queue.Dequeue()
+            let mutable rhs = queue.Dequeue() |> int64
+            while queue.TryPeek(&next) && precedence.[next] > precedence.[op] do
+                rhs <- solveRec rhs queue precedence.[next]
+            lhs <- match op with
+                   | "+" -> lhs + rhs
+                   | "*" -> lhs * rhs
+        lhs
 
-    for i in 1 .. items.Length - 1 do
-        match items.[i] with
-        | "+" -> op <- (+)
-        | "*" -> op <- (*)
-        | n -> value <- op value (int64 n)
+    let mutable queue = new Queue<string>()
+    eq.Split(" ") |> Array.iter (fun n -> queue.Enqueue(n))
+    solveRec (queue.Dequeue() |> int64) queue 0
 
-    value
-
-let solve (input: string) =
+let solve solver (input: string)  =
     let mutable eq = input
     let pattern = "\([\s\+\*\d]+\)"
 
     let solveGroup (group: string) =
-        let n = solveEq (group.TrimStart('(').TrimEnd(')'))
+        let n = solver (group.TrimStart('(').TrimEnd(')'))
         sprintf "%d" n
 
     while Regex.IsMatch(eq, pattern) do
         eq <- Regex.Replace(eq, pattern, fun m -> solveGroup m.Value)
 
-    solveEq eq
+    solver eq
 
-let partOne (input: string list) =
-    let solved = List.map (fun eq -> eq, solve eq) input
+let partOne =
+    let precedence = Map.ofList [("+", 1); ("*", 1)]
+    let solver = solveEq precedence
+    List.sumBy (solve solver)
 
-    solved |> List.iter (fun (eq, ans) -> printfn "%s = %d" eq ans)
-    solved |> List.sumBy snd
+let partTwo =
+    let precedence = Map.ofList [("+", 2); ("*", 1)]
+    let solver = solveEq precedence
+    List.sumBy (solve solver)
 
 [<EntryPoint>]
 let main argv =
     let input = 
         File.ReadLines("Input.txt") 
         |> Seq.toList
-
-    let examples = [
-        "2 * 3 + (4 * 5)"
-        "5 + (8 * 3 + 9 + 3 * 4 * 3)"
-        "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
-        "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
-    ]
         
     partOne input |> printfn "Part one: %d"
-    // partTwo input |> printfn "Part two: %d"
+    partTwo input |> printfn "Part two: %d"
     0
