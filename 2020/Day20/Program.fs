6f474b68bd0aabb0343bc2f0e1c1643222f44ff5
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-20 08:06:24 +0100

    2020 day 20 part 1

---
+++ 2020/Day20/Program.fs
@@ -0,0 +1,63 @@
+﻿module Day20
+
+open System.IO
+
+let parse (tile: string array) =
+    let id = tile.[0].Substring(5, 4) |> int
+    let grid =
+        tile
+        |> Array.skip 1
+        |> Array.map (Seq.map (fun c -> if c = '#' then 1 else 0))
+        |> Array.map Seq.toArray
+
+    (id, grid)
+
+let edges tile =
+    [
+        Array.head tile
+        Array.last tile
+        Array.map (Array.head) tile
+        Array.map (Array.last) tile
+    ]
+
+let edgeId edge =
+    let id = Array.indexed >> Array.sumBy (fun (i, n) -> n <<< i)
+    min (id edge) (Array.rev edge |> id)
+
+let partOne tiles =
+    let mutable edgeToTiles = Map.empty
+    let mutable tileToEdges = Map.empty
+
+    for (tileId, tile) in tiles do
+        let mutable edgeIds = []
+        for edge in edges tile do
+            let eId = edgeId edge
+            let tiles' = Map.tryFind eId edgeToTiles |> Option.defaultValue []
+            edgeIds <- eId :: edgeIds
+            edgeToTiles <- edgeToTiles.Add(eId, tileId :: tiles')
+        tileToEdges <- tileToEdges.Add(tileId, edgeIds)
+
+    let edgeHasMatch e = List.length edgeToTiles.[e] = 2
+    let isCorner edges = List.filter edgeHasMatch edges |> List.length = 2
+    
+    let corners = 
+        tileToEdges 
+        |> Map.filter (fun _ edges -> isCorner edges) 
+        |> Map.toList 
+        |> List.map fst
+
+    corners 
+    |> List.map int64 
+    |> List.fold (*) 1L
+
+[<EntryPoint>]
+let main argv =
+    let input = 
+        File.ReadLines("Input.txt") 
+        |> Seq.filter (fun l -> l <> "")
+        |> Seq.chunkBySize 11
+        |> Seq.map parse
+        |> Seq.toList
+        
+    partOne input |> printfn "Part one: %d"
+    0
