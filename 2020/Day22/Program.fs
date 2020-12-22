259cf32d91ee3f10762d0222287052fa8f4b1723
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-22 09:03:44 +0100

    Clean up 2020 day 22

--- 2020/Day22/Program.fs
+++ 2020/Day22/Program.fs
@@ -16,22 +16,22 @@ let rec play player1 player2 =
         play deck1 (deck2 @ [card2; card1])
 
 let rec play2 player1 player2 =
-    let rec playRound deck1 deck2 cache1 cache2 =
-        let cache1', cache2' = (deck1 :: cache1), (deck2 :: cache2)
+    let rec playRound deck1 deck2 cache =
+        let cache' = deck1 :: cache
         match deck1, deck2 with
-        | d1, d2 when List.contains d1 cache1 && List.contains d2 cache2 -> Player1 d1
+        | d1, _ when List.contains d1 cache -> Player1 d1
         | [], d2 -> Player2 d2
         | d1, [] -> Player1 d1
         | card1 :: d1, card2 :: d2 when card1 <= List.length d1 && card2 <= List.length d2 ->
             match play2 (List.take card1 d1) (List.take card2 d2) with
-            | Player1 _ -> playRound (d1 @ [card1; card2]) d2 cache1' cache2'
-            | Player2 _ -> playRound d1 (d2 @ [card2; card1]) cache1' cache2'
+            | Player1 _ -> playRound (d1 @ [card1; card2]) d2 cache'
+            | Player2 _ -> playRound d1 (d2 @ [card2; card1]) cache'
         | card1 :: d1, card2 :: d2 when card1 > card2 ->
-            playRound (d1 @ [card1; card2]) d2 cache1' cache2'
+            playRound (d1 @ [card1; card2]) d2 cache'
         | card1 :: d1, card2 :: d2 when card2 > card1 ->
-            playRound d1 (d2 @ [card2; card1]) cache1' cache2'
+            playRound d1 (d2 @ [card2; card1]) cache'
 
-    playRound player1 player2 [] []
+    playRound player1 player2 []
 
 let score deck =
     List.rev deck 
@@ -40,12 +40,11 @@ let score deck =
 
 [<EntryPoint>]
 let main argv =
-    let input = 
-        File.ReadLines("Input.txt") 
-        |> Seq.toList
-
-    let player1 = input |> List.skip 1 |> List.takeWhile (fun s -> s <> "") |> List.map int
-    let player2 = input |> List.skip (List.length player1 + 3) |> List.map int
+    let player1, player2 =
+        File.ReadAllText("Input.txt").Split("\r\n\r\n")
+        |> Array.map (fun s -> s.Split("\r\n"))
+        |> Array.map (Array.skip 1 >> Array.map int >> Array.toList)
+        |> fun players -> players.[0], players.[1]
 
     match play player1 player2 with
     | Player1 deck -> printfn "Part one: Player 1 won with score %d" (score deck)
