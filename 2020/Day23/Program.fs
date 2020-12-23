91d7b5cb475495d0e3521cc77e2ba541b2dd1ce5
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-23 09:51:12 +0100

    2020 day 23 refactor
    Storing links in an array instead of map makes it much, much faster.

--- 2020/Day23/Program.fs
+++ 2020/Day23/Program.fs
@@ -1,49 +1,62 @@
 ﻿open System
 
-let play cups moves: seq<int> =
+let play cups moves =
     let maxCup = List.max cups
     let nextCup cup = if cup = 1 then maxCup else cup - 1
 
-    let mutable links = cups |> Seq.pairwise |> Map.ofSeq
-    links <- links.Add(Seq.last cups, Seq.head cups)
+    let mutable links = Array.init (moves + 1) (fun _ -> -1)
+    links.[Seq.last cups] <- Seq.head cups
+    for l, r in Seq.pairwise cups do
+        links.[l] <- r
 
     let mutable current = Seq.head cups
-
     for _ in 0 .. moves - 1 do
         // Take out three cups next to current
-        let mutable pick = [links.[current]]
+        let mutable picked = [links.[current]]
         for _ in 0 .. 1 do
-            pick <- pick @ [links.[List.last pick]]
+            picked <- picked @ [links.[List.last picked]]
 
         // Link current to the cup after the picked three
-        links <- links.Add(current, (links.[List.last pick]))
+        links.[current] <- links.[List.last picked]
 
         // Select destination cup
-        let mutable dest = nextCup current
-        while List.contains dest pick do
-            dest <- nextCup dest
+        let mutable destination = nextCup current
+        while List.contains destination picked do
+            destination <- nextCup destination
 
         // Place three cups after destination
-        let destLink = links.[dest]
-        links <- links.Add(dest, List.head pick).Add(List.last pick, destLink)
+        let nextToDest = links.[destination]
+        links.[destination] <- List.head picked
+        links.[List.last picked] <- nextToDest
         current <- links.[current]
 
     seq {
-        let mutable pointer = 1
-        while links.[pointer] <> 1 do
-            yield links.[pointer]
-            pointer <- links.[pointer]
+        let mutable cup = 1
+        yield cup
+        while links.[cup] <> 1 do
+            yield links.[cup]
+            cup <- links.[cup]
     }
 
 [<EntryPoint>]
 let main argv =
-    let input = "586439172" |> Seq.map (fun c -> Char.GetNumericValue(c) |> int) |> Seq.toList
+    let input = 
+        "586439172" 
+        |> Seq.map (fun c -> Char.GetNumericValue(c) |> int) 
+        |> Seq.toList
 
-    let partOne = play input 100 |> Seq.takeWhile (fun n -> n <> 1) |> Seq.fold (sprintf "%s%d") ""
-    printfn "Part one: %s" partOne
+    play input 100 
+    |> Seq.skip 1 
+    |> Seq.fold (sprintf "%s%d") ""
+    |> printfn "Part one: %s"
 
-    let bigInput = Seq.append input (seq { 10 .. 1_000_000 }) |> Seq.toList
-    let partTwo = play bigInput 10_000_000 |> Seq.take 2 |> Seq.map int64 |> Seq.fold (*) 1L
-    printfn "Part two: %d" partTwo
+    let bigInput = input @ Seq.toList (seq { 10 .. 1_000_000 })
+    
+    play bigInput 10_000_000
+    |> Seq.skip 1 
+    |> Seq.take 2 
+    |> Seq.map int64 
+    |> Seq.fold (*) 1L
+    |> printfn "Part two: %d"
 
     0
