d4453fa76c134e9c69f482c4fd7784fc08faaca9
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-25 08:04:07 +0100

    2020 day 25. Merry Christmas!

---
+++ 2020/Day25/Program.fs
@@ -0,0 +1,36 @@
+﻿module Day25
+
+open System.IO
+
+let transform x subjectNumber = (x * subjectNumber) % 20201227L
+
+let findLoopSize publicKey =
+    let mutable loops = 0
+    let mutable result = 1L
+
+    while result <> publicKey do
+        result <- transform result 7L
+        loops <- loops + 1
+
+    loops
+
+let findEncryptionKey publicKey loopSize =
+    let mutable key = 1L
+
+    for _ in 1 .. loopSize do
+        key <- transform key publicKey
+
+    key
+
+[<EntryPoint>]
+let main argv =
+    let pk1, pk2 = 
+        File.ReadLines("Input.txt") 
+        |> Seq.map int64
+        |> Seq.toList
+        |> fun s -> s.[0], s.[1]
+
+    let ls1, ls2 = findLoopSize pk1, findLoopSize pk2
+    let ek1, ek2 = findEncryptionKey pk1 ls2, findEncryptionKey pk2 ls1
+    printfn "EncryptionKey1: %d, EncryptionKey2: %d" ek1 ek2
+    0
