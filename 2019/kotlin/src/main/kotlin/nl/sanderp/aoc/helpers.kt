9b87de47294d6d9ccf1ec10214b84bb798cc87cd
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-15 15:53:36 +0100

    2019 day 7 in Kotlin

--- 2019/kotlin/src/main/kotlin/nl/sanderp/aoc/helpers.kt
+++ 2019/kotlin/src/main/kotlin/nl/sanderp/aoc/helpers.kt
@@ -7,3 +7,17 @@ object IO {
     fun readLines(name: String) = readText(name).split(System.lineSeparator())
     fun <T> readLines(name: String, parse: (String) -> T) = readLines(name).map(parse)
 }
+
+fun <T> List<T>.permutations(): List<List<T>> {
+    if (this.size == 1) return listOf(this)
+    val perms = mutableListOf<List<T>>()
+    val toInsert = this[0]
+    for (perm in this.drop(1).permutations()) {
+        for (i in 0..perm.size) {
+            val newPerm = perm.toMutableList()
+            newPerm.add(i, toInsert)
+            perms.add(newPerm)
+        }
+    }
+    return perms
+}
