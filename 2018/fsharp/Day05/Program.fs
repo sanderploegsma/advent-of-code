fbfa83dc5454384ab221d2e416e7f558dd64c7b5
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-15 15:47:31 +0100

    2018 day 5 in F#

---
+++ 2018/fsharp/Day05/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open System.IO
+
+let reduce polymer =
+    let react polymer' unit =
+        match polymer' with
+        | x :: tail when abs (unit - x) = 32 -> tail
+        | _ -> unit :: polymer'
+        
+    Seq.fold react [] polymer
+    
+let partOne = Seq.map int >> reduce >> Seq.length
+
+let partTwo polymer =
+    let polymerExcludingUnit unit =
+        polymer
+        |> String.filter (fun c -> Char.ToLower(c) <> unit)
+    
+    polymer
+    |> Seq.distinctBy Char.ToLower
+    |> Seq.map polymerExcludingUnit
+    |> Seq.map partOne
+    |> Seq.min
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadAllText("Input.txt")
+    
+    printfn "Part one: %d" <| partOne input
+    printfn "Part two: %d" <| partTwo input
+    
+    0 // return an integer exit code
