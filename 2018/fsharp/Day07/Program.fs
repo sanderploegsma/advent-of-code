92cf4a40b305adeb50a092cf89943de1bdcb5fe1
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-16 14:45:27 +0100

    2018 day 7 part 2

--- 2018/fsharp/Day07/Program.fs
+++ 2018/fsharp/Day07/Program.fs
@@ -11,30 +11,20 @@ let (|Regex|_|) pattern input =
 
 let parse line =
     match line with
-    | Regex @"Step ([A-Z]) must be finished before step ([A-Z]) can begin." [ a; b ] -> a, b
+    | Regex @"Step ([A-Z]) must be finished before step ([A-Z]) can begin." [ a; b ] -> a.[0], b.[0]
     | _ -> failwithf "Unable to parse line: %s" line
 
-let rec solve (dependencies: Map<string, string list>) readySteps finishedSteps =
-    match Set.toList readySteps |> List.sort with
-    | [] -> finishedSteps
-    | x :: tail ->
-        let finishedSteps' = finishedSteps @ [ x ]
+type Task = { Task: char; TimeLeft: int }
 
-        let unlocked =
-            dependencies
-            |> Map.filter (fun _ -> List.except finishedSteps' >> List.isEmpty)
-            |> Map.toSeq
-            |> Seq.map fst
-            |> Set.ofSeq
-            
-        let newReady = Set.difference (Set.ofList tail |> Set.union unlocked) (Set.ofList finishedSteps')
+type Worker =
+    | Idle
+    | Working of Task
 
-        solve dependencies newReady finishedSteps'
-
-[<EntryPoint>]
-let main argv =
-    let dependencies =
-        File.ReadLines("Input.txt") |> Seq.map parse
+let solve timePerTask numWorkers dependencies =
+    let allTasks =
+        dependencies
+        |> Seq.collect (fun (a, b) -> [ a; b ])
+        |> Set.ofSeq
 
     let dependencyMap =
         dependencies
@@ -42,14 +32,60 @@ let main argv =
         |> Map.ofSeq
         |> Map.map (fun _ values -> Seq.map fst values |> Seq.toList)
 
-    let ready =
-        dependencies
-        |> Seq.map fst
-        |> Seq.filter (fun x -> not (Map.containsKey x dependencyMap))
-        |> Set.ofSeq
+    let mutable finished = List.empty
+    let mutable remaining = Set.toList allTasks
 
-    solve dependencyMap ready []
-    |> String.Concat
-    |> printfn "Part one: %s"
+    let isTaskReady task =
+        Map.tryFind task dependencyMap
+        |> Option.map (List.except finished >> List.isEmpty)
+        |> Option.defaultValue true
+
+    let pullTask () =
+        let readyTasks =
+            remaining |> List.filter isTaskReady |> List.sort
+
+        match readyTasks with
+        | [] -> Idle
+        | task :: _ ->
+            remaining <- List.except [ task ] remaining
+            let time = timePerTask + (int task - int 'A' + 1)
+            Working { Task = task; TimeLeft = time }
+
+    let mutable t = 0
+
+    let maxT =
+        List.length remaining * (timePerTask + 26)
+
+    let schedule = Array2D.create maxT numWorkers Idle
+
+    while not (Set.forall (fun task -> List.contains task finished) allTasks) do
+        for w in 0 .. numWorkers - 1 do
+            if t = 0 then
+                schedule.[t, w] <- pullTask ()
+            else
+                match schedule.[t - 1, w] with
+                | Idle -> schedule.[t, w] <- pullTask ()
+                | Working task when task.TimeLeft = 1 ->
+                    finished <- finished @ [ task.Task ]
+                    schedule.[t, w] <- pullTask ()
+                | Working task ->
+                    schedule.[t, w] <- Working
+                                           { task with
+                                                 TimeLeft = task.TimeLeft - 1 }
+
+        t <- t + 1
+
+    finished, t - 1
+
+[<EntryPoint>]
+let main argv =
+    let dependencies =
+        File.ReadLines("Input.txt") |> Seq.map parse
+
+    let order, _ = solve 0 1 dependencies
+    printfn "Part one: %s" (String.Concat order)
+
+    let _, time = solve 60 5 dependencies
+    printfn "Part two: %d" time
 
     0 // return an integer exit code
