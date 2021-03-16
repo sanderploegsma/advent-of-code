d410f177958b3b136a41ae4a550f264be96c5903
Sander Ploegsma <sanderploegsma@gmail.com>
2021-03-16 17:27:16 +0100

    Rewrite 2020 day 5 in C#
    It suddenly clicked.

--- 2020/csharp/Day05/Solution.cs
+++ 2020/csharp/Day05/Solution.cs
@@ -6,14 +6,14 @@ namespace AdventOfCode2020.Day05
 {
     internal class Solution
     {
-        private const int NumberOfRows = 128;
-        private const int NumberOfColumns = 8;
+        private const int NumberOfRowBits = 7;
+        private const int NumberOfColBits = 3;
 
         private readonly IReadOnlyCollection<int> _seats;
         
         public Solution(IEnumerable<string> input)
         {
-            _seats = input.Select(FindSeat).ToList();
+            _seats = input.Select(ParseSeatId).ToList();
         }
 
         public int PartOne() => _seats.Max();
@@ -30,48 +30,20 @@ namespace AdventOfCode2020.Day05
             return missing.Single();
         }
 
-        private static int FindSeat(string seat)
+        private static int ParseSeatId(string seat)
         {
-            var rows = Enumerable.Range(0, NumberOfRows).ToArray();
-            var columns = Enumerable.Range(0, NumberOfColumns).ToArray();
-            var partitions = seat.Select(ParsePartition).ToArray();
-
-            var row = BinaryTraverse(rows, partitions.Take(7).ToList());
-            var column = BinaryTraverse(columns, partitions.Skip(7).ToList());
-
-            return SeatId(row, column);
+            var id = seat.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
+            return Convert.ToInt32(id, 2);
         }
 
-        private static int BinaryTraverse(IReadOnlyCollection<int> values, IReadOnlyCollection<int> choices)
-        {
-            if (choices.Count == 0)
-                return values.First();
-
-            var choice = choices.First();
-            var nextChoices = choices.Skip(1).ToArray();
-            
-            var middle = values.Count / 2;
-            var (left, right) = (values.Take(middle).ToArray(), values.Skip(middle).ToArray());
-
-            return choice < 0 ? BinaryTraverse(left, nextChoices) : BinaryTraverse(right, nextChoices);
-        }
-
-        private static int ParsePartition(char partition) => partition switch
-        {
-            'F' => -1,
-            'B' => 1,
-            'L' => -1,
-            'R' => 1,
-            _ => throw new ArgumentException($"Invalid partition '{partition}'")
-        };
-        
-        private static int SeatId(int row, int column) => row * 8 + column;
-        
         private static IEnumerable<int> AllPossibleSeats()
         {
-            for (var row = 0; row < NumberOfRows; row++)
-            for (var col = 0; col < NumberOfColumns; col++)
-                yield return SeatId(row, col);
+            var rows = (int) Math.Pow(2, NumberOfRowBits);
+            var cols = (int) Math.Pow(2, NumberOfColBits);
+            
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                yield return (row << NumberOfColBits) + col;
         }
     }
 }
