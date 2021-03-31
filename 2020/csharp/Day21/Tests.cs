f4cf3718724b160ec260946406d68c021283851b
Sander Ploegsma <sanderploegsma@gmail.com>
2021-03-31 12:20:46 +0200

    2020 day 21 C#

---
+++ 2020/csharp/Day21/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace AdventOfCode2020.Day21
+{
+    public class Tests
+    {
+        private const string InputFile = "Day21\\Input.txt";
+        private const string ExampleFile = "Day21\\Example.txt";
+
+        [Theory]
+        [InlineData(ExampleFile, 5)]
+        [InlineData(InputFile, 2098)]
+        public void TestPartOne(string file, int expected)
+        {
+            var solution = new Solution(File.ReadLines(file));
+            Assert.Equal(expected, solution.PartOne());
+        }
+
+        [Theory]
+        [InlineData(ExampleFile, "mxmxvkd,sqjhc,fvjkl")]
+        [InlineData(InputFile, "ppdplc,gkcplx,ktlh,msfmt,dqsbql,mvqkdj,ggsz,hbhsx")]
+        public void TestPartTwo(string file, string expected)
+        {
+            var solution = new Solution(File.ReadLines(file));
+            Assert.Equal(expected, solution.PartTwo());
+        }
+    }
+}
