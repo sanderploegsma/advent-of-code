1430952accb2c26b9acde615952f86df7f077303
Sander Ploegsma <sanderploegsma@gmail.com>
2021-04-06 10:42:56 +0200

    2019 day 1 F#

---
+++ 2019/fsharp/Day01/Solution.fs
@@ -0,0 +1,16 @@
+﻿module AdventOfCode2019.Day01.Solution
+
+let calculateFuelRequirements mass =
+    float mass
+    |> fun x -> x / 3.0
+    |> floor
+    |> fun x -> x - 2.0
+    |> int
+
+let rec calculateFuelRequirementsRec mass =
+    let fuel = calculateFuelRequirements mass
+    if fuel < 0 then 0 else fuel + calculateFuelRequirementsRec fuel
+
+let sumFuelRequirements: seq<int> -> int = Seq.sumBy calculateFuelRequirements
+
+let sumFuelRequirementsRec: seq<int> -> int = Seq.sumBy calculateFuelRequirementsRec
---
+++ 2019/fsharp/Day01/Tests.fs
@@ -0,0 +1,30 @@
+﻿module AdventOfCode2019.Day01.Tests
+
+open AdventOfCode2019.Day01.Solution
+open System.IO
+open Xunit
+
+[<Theory>]
+[<InlineData(12, 2)>]
+[<InlineData(14, 2)>]
+[<InlineData(1969, 654)>]
+[<InlineData(100756, 33583)>]
+let ``Part One - Examples`` mass expected =
+    Assert.Equal(expected, calculateFuelRequirements mass)
+
+[<Fact>]
+let ``Part One``() =
+    let input = File.ReadLines("Day01\\Input.txt") |> Seq.map int
+    Assert.Equal(3361976, sumFuelRequirements input)
+    
+[<Theory>]
+[<InlineData(14, 2)>]
+[<InlineData(1969, 966)>]
+[<InlineData(100756, 50346)>]
+let ``Part Two - Examples`` mass expected =
+    Assert.Equal(expected, calculateFuelRequirementsRec mass)
+
+[<Fact>]
+let ``Part Two``() =
+    let input = File.ReadLines("Day01\\Input.txt") |> Seq.map int
+    Assert.Equal(5040085, sumFuelRequirementsRec input)
