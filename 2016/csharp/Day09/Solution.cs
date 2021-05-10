1fda49d3101fe7a009ebdcc9be8f3c04ac293aac
Sander Ploegsma <sanderploegsma@gmail.com>
2021-05-10 12:15:29 +0200

    2016 day 9 C#

---
+++ 2016/csharp/Day09/Solution.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2016.Day09
+{
+    internal class Solution
+    {
+        private readonly string _input;
+
+        public Solution(string input)
+        {
+            _input = input;
+        }
+        
+        public long PartOne()
+        {
+            var result = "";
+            var i = 0;
+
+            while (i < _input.Length)
+            {
+                if (_input[i] == '(')
+                {
+                    var j = _input.IndexOf(')', i);
+                    var marker = _input[i..j];
+                    var (count, repeat) = ParseMarker(marker);
+                    var sequence = _input.Substring(j + 1, count);
+
+                    result += string.Concat(Enumerable.Repeat(sequence, repeat));
+                    i = j + count + 1;
+                }
+                else
+                {
+                    result += _input[i];
+                    i++;
+                }
+            }
+
+            return result.LongCount();
+        }
+
+        public long PartTwo() => GetLength(_input);
+
+        private static long GetLength(string compressed)
+        {
+            var length = 0L;
+            var i = 0;
+
+            while (i < compressed.Length)
+            {
+                if (compressed[i] == '(')
+                {
+                    var j = compressed.IndexOf(')', i);
+                    var marker = compressed[i..j];
+                    var (count, repeat) = ParseMarker(marker);
+                    var sequenceLength = GetLength(compressed.Substring(j + 1, count));
+
+                    length += sequenceLength * repeat;
+                    i = j + count + 1;
+                }
+                else
+                {
+                    i++;
+                    length++;
+                }
+            }
+
+            return length;
+        }
+
+        private static (int Count, int Repeat) ParseMarker(string marker)
+        {
+            var segments = marker.Trim('(', ')').Split('x').Select(int.Parse).ToArray();
+            return (segments[0], segments[1]);
+        }
+    }
+}
---
+++ 2016/csharp/Day09/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace AdventOfCode2016.Day09
+{
+    public class Tests
+    {
+        private const string InputFile = @"Day09\\Input.txt";
+
+        [Theory]
+        [MemberData(nameof(PartOneData))]
+        public void PartOne(string input, long expected)
+        {
+            var solution = new Solution(input);
+            Assert.Equal(expected, solution.PartOne());
+        }
+
+        [Theory]
+        [MemberData(nameof(PartTwoData))]
+        public void PartTwo(string input, long expected)
+        {
+            var solution = new Solution(input);
+            Assert.Equal(expected, solution.PartTwo());
+        }
+
+        public static IEnumerable<object[]> PartOneData()
+        {
+            yield return new object[] {"ADVENT", 6L};
+            yield return new object[] {"A(1x5)BC", 7L};
+            yield return new object[] {"(3x3)XYZ", 9L};
+            yield return new object[] {"A(2x2)BCD(2x2)EFG", 11L};
+            yield return new object[] {"(6x1)(1x3)A", 6L};
+            yield return new object[] {"X(8x2)(3x3)ABCY", 18L};
+            yield return new object[] {File.ReadAllText(InputFile), 70186L};
+        }
+        
+        public static IEnumerable<object[]> PartTwoData()
+        {
+            yield return new object[] {"(3x3)XYZ", 9L};
+            yield return new object[] {"X(8x2)(3x3)ABCY", 20L};
+            yield return new object[] {"(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920L};
+            yield return new object[] {"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445L};
+            yield return new object[] {File.ReadAllText(InputFile), 10915059201L};
+        }
+    }
+}
