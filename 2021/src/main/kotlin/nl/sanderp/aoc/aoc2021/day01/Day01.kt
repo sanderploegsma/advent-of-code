7bf3d2c38037b78939bfc2a6e16736f03c3ff3fb
Sander Ploegsma <sanderploegsma@gmail.com>
2021-12-01 07:42:37 +0100

    2021 day 1

--- 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/day01/Day01.kt
+++ 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/day01/Day01.kt
@@ -4,18 +4,19 @@ import nl.sanderp.aoc.aoc2021.measureDuration
 import nl.sanderp.aoc.aoc2021.prettyPrint
 import nl.sanderp.aoc.aoc2021.readResource
 
-class Day01(private val input: String) {
-    fun partOne() = input
-    fun partTwo() = input.reversed()
-}
-
 fun main() {
     val input = readResource("Day01.txt")
-    val solver = Day01(input)
+    val measurements = input.lines().map { it.toInt() }
 
-    val (answer1, duration1) = measureDuration<String> { solver.partOne() }
+    val (answer1, duration1) = measureDuration<Int> {
+        measurements.zipWithNext().count { (a, b) -> b > a }
+    }
+
     println("Part one: $answer1 (took ${duration1.prettyPrint()})")
 
-    val (answer2, duration2) = measureDuration<String> { solver.partTwo() }
+    val (answer2, duration2) = measureDuration<Int> {
+        measurements.windowed(3, 1).zipWithNext().count { (a, b) -> b.sum() > a.sum() }
+    }
+
     println("Part two: $answer2 (took ${duration2.prettyPrint()})")
 }
