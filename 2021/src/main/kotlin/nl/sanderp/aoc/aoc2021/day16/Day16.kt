743136e5514c6fa1585a5aae7a69e1128b3641e8
Sander Ploegsma <sanderploegsma@gmail.com>
2021-12-16 13:33:18 +0100

    2021 day 16 refactor

--- 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/day16/Day16.kt
+++ 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/day16/Day16.kt
@@ -19,46 +19,25 @@ private sealed class Packet(val version: Int) {
 }
 
 private class LiteralValue(version: Int, override val value: Long) : Packet(version) {
-    override val versions: List<Int>
-        get() = listOf(version)
+    override val versions: List<Int> = listOf(version)
 }
 
-private abstract class Operator(version: Int, val packets: List<Packet>) : Packet(version) {
+private enum class OperatorType { Sum, Product, Minimum, Maximum, GreaterThan, LessThan, EqualTo }
+
+private class Operator(version: Int, val type: OperatorType, val packets: List<Packet>) : Packet(version) {
     override val versions: List<Int>
         get() = listOf(version).plus(packets.flatMap { it.versions })
 
-    override val value: Long
-        get() = calculateValue()
-
-    abstract fun calculateValue(): Long
-}
-
-private class Sum(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = packets.sumOf { it.value }
-}
-
-private class Product(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = packets.fold(1L) { p, v -> p * v.value }
-}
-
-private class Minimum(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = packets.minOf { it.value }
-}
-
-private class Maximum(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = packets.maxOf { it.value }
-}
-
-private class GreaterThan(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = if (packets[0].value > packets[1].value) 1L else 0L
-}
-
-private class LessThan(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = if (packets[0].value < packets[1].value) 1L else 0L
-}
-
-private class EqualTo(version: Int, packets: List<Packet>) : Operator(version, packets) {
-    override fun calculateValue() = if (packets[0].value == packets[1].value) 1L else 0L
+    override val value
+        get() = when (type) {
+            OperatorType.Sum -> packets.sumOf { it.value }
+            OperatorType.Product -> packets.fold(1L) { p, v -> p * v.value }
+            OperatorType.Minimum -> packets.minOf { it.value }
+            OperatorType.Maximum -> packets.maxOf { it.value }
+            OperatorType.GreaterThan -> if (packets[0].value > packets[1].value) 1L else 0L
+            OperatorType.LessThan -> if (packets[0].value < packets[1].value) 1L else 0L
+            OperatorType.EqualTo -> if (packets[0].value == packets[1].value) 1L else 0L
+        }
 }
 
 private class Reader(data: String) {
@@ -66,17 +45,16 @@ private class Reader(data: String) {
 
     fun next(): Packet {
         val version = readBits(3).toInt(2)
-        val typeId = readBits(3).toInt(2)
 
-        return when (typeId) {
-            0 -> Sum(version, readOperatorPackets())
-            1 -> Product(version, readOperatorPackets())
-            2 -> Minimum(version, readOperatorPackets())
-            3 -> Maximum(version, readOperatorPackets())
+        return when (val typeId = readBits(3).toInt(2)) {
+            0 -> Operator(version, OperatorType.Sum, readOperatorPackets())
+            1 -> Operator(version, OperatorType.Product, readOperatorPackets())
+            2 -> Operator(version, OperatorType.Minimum, readOperatorPackets())
+            3 -> Operator(version, OperatorType.Maximum, readOperatorPackets())
             4 -> LiteralValue(version, readLiteralValue())
-            5 -> GreaterThan(version, readOperatorPackets())
-            6 -> LessThan(version, readOperatorPackets())
-            7 -> EqualTo(version, readOperatorPackets())
+            5 -> Operator(version, OperatorType.GreaterThan, readOperatorPackets())
+            6 -> Operator(version, OperatorType.LessThan, readOperatorPackets())
+            7 -> Operator(version, OperatorType.EqualTo, readOperatorPackets())
             else -> throw IllegalArgumentException("Invalid type ID '$typeId'")
         }
     }
@@ -94,26 +72,22 @@ private class Reader(data: String) {
         return bits.toLong(2)
     }
 
-    private fun readOperatorPackets(): List<Packet> {
-        if (readBits(1) == "0") {
+    private fun readOperatorPackets(): List<Packet> = when (readBits(1)) {
+        "0" -> buildList {
             val length = readBits(15).toLong(2)
             val bufferStopSize = buffer.length - length
-
-            return buildList {
-                while (buffer.length > bufferStopSize) {
-                    add(next())
-                }
-            }
-        } else {
-            val packets = readBits(11).toLong(2)
-            return buildList {
-                for (i in 1..packets) {
-                    add(next())
-                }
+            while (buffer.length > bufferStopSize) {
+                add(next())
             }
         }
+        "1" -> buildList {
+            val packets = readBits(11).toLong(2)
+            for (i in 1..packets) {
+                add(next())
+            }
+        }
+        else -> throw IllegalArgumentException()
     }
 
     private fun readBits(n: Int) = buffer.take(n).also { buffer = buffer.drop(n) }
 }
-
