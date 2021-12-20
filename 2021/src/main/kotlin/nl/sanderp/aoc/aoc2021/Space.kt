a545377d8baf67fdb586e22df248bc6260a796ed
Sander Ploegsma <sanderploegsma@gmail.com>
2021-12-20 18:00:32 +0100

    2021 day 20

--- 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/Space.kt
+++ 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/Space.kt
@@ -26,8 +26,8 @@ fun Point2D.pointsNextTo() = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1).map { this
 fun Point2D.pointsAround() = (-1..1).allPairs().filterNot { it == Pair(0, 0) }.map { this + it }.toList()
 
 fun Iterable<Point2D>.print() {
-    for (y in 0..this.maxOf { it.y }) {
-        for (x in 0..this.maxOf { it.x }) {
+    for (y in this.minOf { it.y }..this.maxOf { it.y }) {
+        for (x in this.minOf { it.x }..this.maxOf { it.x }) {
             if (this.contains(x to y)) {
                 print('█')
             } else {
---
+++ 2021/src/main/kotlin/nl/sanderp/aoc/aoc2021/day20/Day20.kt
@@ -0,0 +1,59 @@
+package nl.sanderp.aoc.aoc2021.day20
+
+import nl.sanderp.aoc.aoc2021.Point2D
+import nl.sanderp.aoc.aoc2021.readResource
+import nl.sanderp.aoc.aoc2021.x
+import nl.sanderp.aoc.aoc2021.y
+
+fun main() {
+    val input = readResource("Day20.txt").replace('.', '0').replace('#', '1').lines()
+    val algorithm = input.first()
+    val image = parseImage(input.drop(2))
+
+    val enhanced1 = image.enhance(algorithm, 2)
+    println("Part one: ${enhanced1.pixels.count { it.value == '1' }}")
+
+    val enhanced2 = image.enhance(algorithm, 50)
+    println("Part two: ${enhanced2.pixels.count { it.value == '1' }}")
+}
+
+private data class Image(val pixels: Map<Point2D, Char>, val default: Char) {
+    val minX = pixels.minOf { it.key.x }
+    val minY = pixels.minOf { it.key.y }
+    val maxX = pixels.maxOf { it.key.x }
+    val maxY = pixels.maxOf { it.key.y }
+}
+
+private fun parseImage(image: List<String>): Image {
+    val pixels = buildMap {
+        for ((y, row) in image.withIndex()) {
+            for ((x, c) in row.withIndex()) {
+                put(x to y, c)
+            }
+        }
+    }
+
+    return Image(pixels, '0')
+}
+
+private fun Image.enhance(algorithm: String, times: Int) =
+    generateSequence(this) { it.enhance(algorithm) }.drop(times).first()
+
+private fun Image.enhance(algorithm: String): Image {
+    val nextPixels = mutableMapOf<Point2D, Char>()
+    for (nextY in minY - 1..maxY + 1) {
+        for (nextX in minX - 1..maxX + 1) {
+            var bits = ""
+
+            for (y in nextY - 1..nextY + 1) {
+                for (x in nextX - 1..nextX + 1) {
+                    bits += pixels[Point2D(x, y)] ?: default
+                }
+            }
+
+            nextPixels[nextX to nextY] = algorithm[bits.toInt(2)]
+        }
+    }
+
+    return Image(nextPixels, default = algorithm[default.toString().repeat(9).toInt(2)])
+}
