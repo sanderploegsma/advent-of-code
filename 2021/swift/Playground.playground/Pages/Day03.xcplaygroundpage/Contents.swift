57b573ab99051ec52bce2ec0b198742c38a1ceb6
Sander Ploegsma <sanderploegsma@gmail.com>
2022-01-12 12:46:33 +0100

    2021 day 3 in Swift

---
+++ 2021/swift/Playground.playground/Pages/Day03.xcplaygroundpage/Contents.swift
@@ -0,0 +1,62 @@
+//: [Previous](@previous)
+
+import Algorithms
+import Foundation
+
+let input = readLines(fromResource: "Input")
+
+var gammaRate: [Character] = []
+var epsilonRate: [Character] = []
+
+for position in 0...11 {
+    let (ones, zeroes) = input.partitioned(by: { $0[position] == "0" })
+
+    if ones.count > zeroes.count {
+        gammaRate.append("1")
+        epsilonRate.append("0")
+    } else {
+        gammaRate.append("0")
+        epsilonRate.append("1")
+    }
+}
+
+let gammaRateValue = Int(String(gammaRate), radix: 2)!
+let epsilonRateValue = Int(String(epsilonRate), radix: 2)!
+
+print("Part one: \(gammaRateValue * epsilonRateValue)")
+
+var oxygenGeneratorRating = input
+var co2ScrubberRating = input
+
+for position in 0...11 {
+    if oxygenGeneratorRating.count > 1 {
+        let (ones, zeroes) = oxygenGeneratorRating.partitioned(by: { $0[position] == "0" })
+        if ones.count >= zeroes.count {
+            oxygenGeneratorRating = ones
+        } else {
+            oxygenGeneratorRating = zeroes
+        }
+    }
+
+    if co2ScrubberRating.count > 1 {
+        let (ones, zeroes) = co2ScrubberRating.partitioned(by: { $0[position] == "0" })
+        if zeroes.count <= ones.count {
+            co2ScrubberRating = zeroes
+        } else {
+            co2ScrubberRating = ones
+        }
+    }
+}
+
+let oxygenGeneratorRatingValue = Int(oxygenGeneratorRating.first!, radix: 2)!
+let co2ScrubberRatingValue = Int(co2ScrubberRating.first!, radix: 2)!
+
+print("Part two: \(oxygenGeneratorRatingValue * co2ScrubberRatingValue)")
+
+extension StringProtocol {
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
+    }
+}
+
+//: [Next](@next)
