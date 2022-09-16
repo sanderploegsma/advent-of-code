98bdf24af6e78acfbd40b783b1b16946efd4da22
Sander Ploegsma <sanderploegsma@gmail.com>
2022-09-16 15:11:16 +0200

    2020 day 1 in python

---
+++ 2020/python/day01.py
@@ -0,0 +1,14 @@
+from functools import reduce
+from itertools import permutations
+from operator import mul
+
+input = open('day01.txt').read()
+numbers = [int(x) for x in input.splitlines()]
+
+def find_numbers_with_total_sum(total, n):
+    candidates = permutations(numbers, n)
+    matches = filter(lambda x: sum(x) == total, candidates)
+    return next(matches)
+
+print('part one: %d' % reduce(mul, find_numbers_with_total_sum(2020, 2)))
+print('part two: %d' % reduce(mul, find_numbers_with_total_sum(2020, 3)))
