446fb0e76e5ba0fe83019e14b855983f128cc8e8
Sander Ploegsma <sanderploegsma@gmail.com>
2022-09-16 15:43:56 +0200

    2020 day 3 in python

---
+++ 2020/python/day03.py
@@ -0,0 +1,22 @@
+from functools import reduce
+from operator import mul
+
+input = open('day03.txt').read().splitlines()
+
+def traverse(step_x, step_y):
+    """
+    Traverses the input using the given step sizes and counts the number of trees on the traversed path.
+    """
+    x, y, trees = 0, 0, 0
+
+    while y < len(input):
+        line = input[y]
+        if line[x % len(line)] == '#':
+            trees += 1
+        x += step_x
+        y += step_y
+
+    return trees
+
+print('part one: %d' % traverse(3, 1))
+print('part two: %d' % reduce(mul, [traverse(*step) for step in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]))
