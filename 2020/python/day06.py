f4b713517ed45183f838dab5ad708ae23e727960
Sander Ploegsma <sanderploegsma@gmail.com>
2022-09-19 10:26:35 +0200

    2020 day 6 in python

---
+++ 2020/python/day06.py
@@ -0,0 +1,29 @@
+"""Advent of Code 2020 - day 06."""
+
+import os
+from typing import List
+
+
+def union(group: List[List[str]]) -> List[str]:
+    result = []
+    for answers in group:
+        result = set(result) | set(answers)
+
+    return result
+
+
+def intersect(group: List[List[str]]) -> List[str]:
+    result = union(group)
+
+    for answers in group:
+        result = set(result) & set(answers)
+
+    return result
+
+
+with open('day06.txt', encoding='utf8') as file:
+    groups = [group.splitlines()
+              for group in file.read().split(os.linesep + os.linesep)]
+
+    print(f"part one: {sum(len(union(group)) for group in groups)}")
+    print(f"part two: {sum(len(intersect(group)) for group in groups)}")
