62d7e7921fdac8b558755482e266dd261fb39bea
Sander Ploegsma <sanderploegsma@gmail.com>
2022-12-01 07:39:28 +0100

    2022 day 1

--- 2022/csharp/Day01.cs
+++ 2022/csharp/Day01.cs
@@ -2,26 +2,53 @@
 
 internal class Day01
 {
-    private readonly string _input;
+    private readonly List<List<int>> _data;
 
     public Day01(string input)
     {
-        _input = input;
+        _data = input
+            .Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList())
+            .ToList();
     }
 
-    public int PartOne() { return _input.Length; }
+    public int PartOne()
+    { 
+        return _data
+            .Select(x => x.Sum())
+            .Max();
+    }
+
+    public int PartTwo()
+    {
+        return _data
+            .Select(x => x.Sum())
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+    }
 }
 
 public class Day01Test
 {
     private const string InputFile = "Day01.Input.txt";
+    private const string ExampleFile = "Day01.Example.txt";
 
     [Theory]
-    [InlineData("OneTwoThree", 11)]
-    [FileData(InputFile, 21)]
+    [FileData(ExampleFile, 24000)]
+    [FileData(InputFile, 69528)]
     public void TestPartOne(string input, int expected)
     {
         var solution = new Day01(input);
         Assert.Equal(expected, solution.PartOne());
+    }
+
+    [Theory]
+    [FileData(ExampleFile, 45000)]
+    [FileData(InputFile, 206152)]
+    public void TestPartTwo(string input, int expected)
+    {
+        var solution = new Day01(input);
+        Assert.Equal(expected, solution.PartTwo());
     }
 }
