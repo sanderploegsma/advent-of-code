f3db0574154835d9dba8b359fc57baf5fef90d86
Sander Ploegsma <sanderploegsma@gmail.com>
2022-12-02 08:01:56 +0100

    2022 day 2

---
+++ 2022/csharp/Day02.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace AdventOfCode2022;
+
+internal class Day02
+{
+    public enum Shape
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3,
+    }
+
+    public enum Result
+    {
+        Loss = 0,
+        Draw = 3,
+        Win = 6,
+    }
+
+    private readonly List<(char, char)> _rounds;
+
+    public Day02(IEnumerable<string> input)
+    {
+        _rounds = input.Select(x => (x[0], x[2])).ToList();
+    }
+
+    public int PartOne()
+    {
+        var scores = from round in _rounds
+                     let theirs = ParseShape(round.Item1)
+                     let mine = ParseShape(round.Item2)
+                     let result = GetResult(theirs, mine)
+                     select GetScore(mine, result);
+
+        return scores.Sum();
+    }
+
+    public int PartTwo()
+    {
+        var scores = from round in _rounds
+                     let theirs = ParseShape(round.Item1)
+                     let result = ParseResult(round.Item2)
+                     from mine in Enum.GetValues<Shape>()
+                     where GetResult(theirs, mine) == result
+                     select GetScore(mine, result);
+
+        return scores.Sum();
+    }
+
+    private static Shape ParseShape(char c) => c switch
+    {
+        'A' or 'X' => Shape.Rock,
+        'B' or 'Y' => Shape.Paper,
+        'C' or 'Z' => Shape.Scissors,
+        _ => throw new ArgumentException(),
+    };
+
+    private static Result ParseResult(char c) => c switch
+    {
+        'X' => Result.Loss,
+        'Y' => Result.Draw,
+        'Z' => Result.Win,
+        _ => throw new ArgumentException(),
+    };
+
+    static Result GetResult(Shape theirs, Shape mine)
+    {
+        if (theirs == mine)
+        {
+            return Result.Draw;
+        }
+
+        if (theirs == Shape.Rock && mine == Shape.Paper ||
+            theirs == Shape.Paper && mine == Shape.Scissors ||
+            theirs == Shape.Scissors && mine == Shape.Rock)
+        {
+            return Result.Win;
+        }
+
+        return Result.Loss;
+    }
+
+    static int GetScore(Shape shape, Result result) => ((int)shape) + ((int)result);
+}
+
+public class Day02Test
+{
+    private const string InputFile = "Day02.Input.txt";
+    private const string ExampleFile = "Day02.Example.txt";
+
+    [Theory]
+    [FileData(ExampleFile, FileContents.StringPerLine, 15)]
+    [FileData(InputFile, FileContents.StringPerLine, 10718)]
+    public void TestPartOne(IEnumerable<string> input, int expected)
+    {
+        var solution = new Day02(input);
+        Assert.Equal(expected, solution.PartOne());
+    }
+
+    [Theory]
+    [FileData(ExampleFile, FileContents.StringPerLine, 12)]
+    [FileData(InputFile, FileContents.StringPerLine, 14652)]
+    public void TestPartTwo(IEnumerable<string> input, int expected)
+    {
+        var solution = new Day02(input);
+        Assert.Equal(expected, solution.PartTwo());
+    }
+}
+
--- 2022/csharp/FileDataAttribute.cs
+++ 2022/csharp/FileDataAttribute.cs
@@ -3,21 +3,40 @@ using Xunit.Sdk;
 
 namespace AdventOfCode2022;
 
+internal enum FileContents
+{
+    SingleString,
+    StringPerLine,
+}
+
 internal class FileDataAttribute : DataAttribute
 {
     private readonly string _filePath;
+    private readonly FileContents _contents;
     private readonly object[] _args;
 
-    public FileDataAttribute(string filePath, params object[] args)
+    public FileDataAttribute(string filePath, params object[] args) : this(filePath, FileContents.SingleString, args)
+    {
+    }
+
+    public FileDataAttribute(string filePath, FileContents contents, params object[] args)
     {
         _filePath = filePath;
+        _contents = contents;
         _args = args;
     }
 
     public override IEnumerable<object[]> GetData(MethodInfo testMethod)
     {
-        var contents = File.ReadAllText(_filePath);
+        var data = ReadFile();
 
-        yield return _args.Prepend(contents).ToArray();
+        yield return _args.Prepend(data).ToArray();
     }
+
+    private object ReadFile() => _contents switch
+    {
+        FileContents.SingleString => File.ReadAllText(_filePath),
+        FileContents.StringPerLine => File.ReadLines(_filePath),
+        _ => throw new ArgumentException(),
+    };
 }
