ab26e16814e9174be60c5b802562a74f57528337
Sander Ploegsma <sanderploegsma@gmail.com>
2023-11-24 16:20:34 +0100

    Rename stub input file

--- 2023/python/aoc_2023/day01.py
+++ 2023/python/aoc_2023/day01.py
@@ -1,4 +1,6 @@
-with open("2023/input/day01.txt", encoding="UTF-8") as f:
+"""Advent of Code 2023 - Day 1."""
+
+with open("2023/input/01.txt", encoding="utf-8") as f:
     input = f.read()
 
-print("Input: ", input)
+print("Input:", input)
