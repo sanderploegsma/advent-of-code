10413b35cb4ecec57c8409f3f4f4a1e31c3bca75
Sander Ploegsma <sanderploegsma@gmail.com>
2023-12-09 16:02:34 +0100

    2023 day 9 refactor

--- 2023/python/aoc_2023/day09.py
+++ 2023/python/aoc_2023/day09.py
@@ -1,18 +1,21 @@
 """Advent of Code 2023 - Day 9."""
 
+from operator import itemgetter
+
 from aoc_2023.input import Input
 
 
-def extrapolate(history: list[int]) -> int:
-    if all(n == 0 for n in history):
-        return 0
+def extrapolate(series: list[int]) -> tuple[int, int]:
+    if all(n == 0 for n in series):
+        return 0, 0
 
-    diffs = list(history[i] - history[i - 1] for i in range(1, len(history)))
-
-    return history[-1] + extrapolate(diffs)
+    diffs = list(series[i] - series[i - 1] for i in range(1, len(series)))
+    diff_left, diff_right = extrapolate(diffs)
+    return series[0] - diff_left, series[-1] + diff_right
 
 
 histories = list(list(map(int, line.split())) for line in Input("09.txt").lines)
+extrapolated = list(map(extrapolate, histories))
 
-print("Part one:", sum(map(extrapolate, histories)))
-print("Part two:", sum(map(extrapolate, [list(reversed(h)) for h in histories])))
+print("Part one:", sum(map(itemgetter(1), extrapolated)))
+print("Part two:", sum(map(itemgetter(0), extrapolated)))
