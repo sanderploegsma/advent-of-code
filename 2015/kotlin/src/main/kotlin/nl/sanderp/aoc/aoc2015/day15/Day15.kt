11bb5352a9999fe902e92f0efc67d39331d31fd5
Sander Ploegsma <sanderploegsma@gmail.com>
2023-12-12 14:16:42 +0100

    2015 day 15 refactor

--- 2015/kotlin/src/main/kotlin/nl/sanderp/aoc/aoc2015/day15/Day15.kt
+++ 2015/kotlin/src/main/kotlin/nl/sanderp/aoc/aoc2015/day15/Day15.kt
@@ -2,49 +2,45 @@ package nl.sanderp.aoc.aoc2015.day15
 
 import kotlin.math.max
 
-data class Properties(val capacity: Int, val durability: Int, val flavor: Int, val texture: Int, val calories: Int) {
-    operator fun plus(other: Properties): Properties = Properties(
-        capacity + other.capacity,
-        durability + other.durability,
-        flavor + other.flavor,
-        texture + other.texture,
-        calories + other.calories,
-    )
-
-    operator fun times(factor: Int): Properties = Properties(
-        capacity * factor,
-        durability * factor,
-        flavor * factor,
-        texture * factor,
-        calories * factor,
-    )
-
-    val score = max(0, capacity) * max(0, durability) * max(0, flavor) * max(0, texture)
+fun Array<Array<Int>>.totals(): Array<Int> {
+    return Array(this[0].size) { j ->
+        this.sumOf { it[j] }
+    }
 }
 
-val ingredients = listOf(
-    Properties(3, 0, 0, -3, 2),
-    Properties(-3, 3, 0, 0, 9),
-    Properties(-1, 0, 4, 0, 1),
-    Properties(0, 0, -2, 2, 8),
-)
-
-fun amounts() = sequence {
-    for (a in 0..100) {
-        for (b in 0..100 - a) {
-            for (c in 0..100 - a - b) {
-                yield(listOf(a, b, c, 100 - a - b - c))
-            }
+operator fun Array<Array<Int>>.times(factors: Array<Int>): Array<Array<Int>> {
+    return Array(size) { i ->
+        Array(this[i].size) { j ->
+            this[i][j] * factors[i]
         }
     }
 }
 
+val ingredients = arrayOf(
+    arrayOf(3, 0, 0, -3, 2),
+    arrayOf(-3, 3, 0, 0, 9),
+    arrayOf(-1, 0, 4, 0, 1),
+    arrayOf(0, 0, -2, 2, 8),
+)
+
+fun score(recipe: Array<Int>) = recipe.dropLast(1).fold(1) { acc, i -> acc * max(0, i) }
+
 fun main() {
-    val recipes = amounts()
-        .map { it.mapIndexed { index, factor -> ingredients[index] * factor } }
-        .map { it.reduce(Properties::plus) }
+    val amounts = sequence {
+        for (a in 0..100) {
+            for (b in 0..100 - a) {
+                for (c in 0..100 - a - b) {
+                    yield(arrayOf(a, b, c, 100 - a - b - c))
+                }
+            }
+        }
+    }
+
+    val recipes = amounts
+        .map { ingredients * it }
+        .map { it.totals() }
         .toList()
 
-    println("Part one: ${recipes.maxOf { it.score }}")
-    println("Part two: ${recipes.filter { it.calories == 500 }.maxOf { it.score }}")
+    println("Part one: ${recipes.maxOf { score(it) }}")
+    println("Part two: ${recipes.filter { it.last() == 500 }.maxOf { score(it) }}")
 }
