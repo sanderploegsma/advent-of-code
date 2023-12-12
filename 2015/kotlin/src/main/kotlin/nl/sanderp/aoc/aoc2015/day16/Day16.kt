b2c80e2b97888859bfb24fbe51c9b3078ea479c4
Sander Ploegsma <sanderploegsma@gmail.com>
2023-12-12 14:43:54 +0100

    2015 day 16 refactor

--- 2015/kotlin/src/main/kotlin/nl/sanderp/aoc/aoc2015/day16/Day16.kt
+++ 2015/kotlin/src/main/kotlin/nl/sanderp/aoc/aoc2015/day16/Day16.kt
@@ -12,36 +12,40 @@ fun parseLine(line: String): Map<String, Int> {
     }
 }
 
+val firstRules = mapOf<String, (Int) -> Boolean>(
+    "children" to { it == 3 },
+    "cats" to { it == 7 },
+    "samoyeds" to { it == 2 },
+    "pomeranians" to { it == 3 },
+    "akitas" to { it == 0 },
+    "vizslas" to { it == 0 },
+    "goldfish" to { it == 5 },
+    "trees" to { it == 3 },
+    "cars" to { it == 2 },
+    "perfumes" to { it == 1 },
+)
+
+val secondRules = firstRules + mapOf(
+    "cats" to { it > 7 },
+    "pomeranians" to { it < 3 },
+    "goldfish" to { it < 5 },
+    "trees" to { it > 3 },
+)
+
+private fun Int?.check(predicate: (Int) -> Boolean) = this?.let(predicate) ?: true
+
 fun main() {
     val input = readResource("16.txt").lines().map(::parseLine)
 
     val firstMatch = input.withIndex().single { (_, facts) ->
-        facts["children"]?.let { it == 3 } ?: true &&
-                facts["cats"]?.let { it == 7 } ?: true &&
-                facts["samoyeds"]?.let { it == 2 } ?: true &&
-                facts["pomeranians"]?.let { it == 3 } ?: true &&
-                facts["akitas"]?.let { it == 0 } ?: true &&
-                facts["vizslas"]?.let { it == 0 } ?: true &&
-                facts["goldfish"]?.let { it == 5 } ?: true &&
-                facts["trees"]?.let { it == 3 } ?: true &&
-                facts["cars"]?.let { it == 2 } ?: true &&
-                facts["perfumes"]?.let { it == 1 } ?: true
+        firstRules.all { (key, predicate) -> facts[key].check(predicate) }
     }
 
     println("Part one: ${firstMatch.index + 1}")
 
     val secondMatch = input.withIndex().single { (_, facts) ->
-        facts["children"]?.let { it == 3 } ?: true &&
-                facts["cats"]?.let { it > 7 } ?: true &&
-                facts["samoyeds"]?.let { it == 2 } ?: true &&
-                facts["pomeranians"]?.let { it < 3 } ?: true &&
-                facts["akitas"]?.let { it == 0 } ?: true &&
-                facts["vizslas"]?.let { it == 0 } ?: true &&
-                facts["goldfish"]?.let { it < 5 } ?: true &&
-                facts["trees"]?.let { it > 3 } ?: true &&
-                facts["cars"]?.let { it == 2 } ?: true &&
-                facts["perfumes"]?.let { it == 1 } ?: true
+        secondRules.all { (key, predicate) -> facts[key].check(predicate) }
     }
 
-    println("Part one: ${secondMatch.index + 1}")
+    println("Part two: ${secondMatch.index + 1}")
 }
