571e93751149c3e1b78fc6674b5acae778988e03
Sander Ploegsma <sanderploegsma@gmail.com>
2023-12-14 23:23:26 +0100

    Update templates

--- 2023/python/aoc_2023/scripts/generator.py
+++ 2023/python/aoc_2023/scripts/generator.py
@@ -12,7 +12,7 @@ TEMPLATE_DIR = os.path.join(SCRIPT_DIR, "templates")
 OUTPUT_DIR = os.path.join(SCRIPT_DIR, "..")
 
 
-def generate(day: str) -> None:
+def generate(**kwargs) -> None:
     env = Environment(autoescape=False, optimized=False)
 
     def generate_file(template_name: str, output_name: str):
@@ -20,22 +20,20 @@ def generate(day: str) -> None:
             template = env.from_string(f.read())
 
         with open(os.path.join(OUTPUT_DIR, output_name), "x", encoding="utf-8") as f:
-            template.stream(day=day).dump(f)
+            template.stream(**kwargs).dump(f)
 
+    day = kwargs["day"]
     generate_file("input.txt.j2", f"day{day}.txt")
     generate_file("solution.py.j2", f"day{day}.py")
     generate_file("test.py.j2", f"day{day}_test.py")
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Please supply a day number")
+    if len(sys.argv) < 3:
+        print("Usage: generate <day> <title of puzzle>")
         exit(1)
 
-    day = int(sys.argv[1])
-    day_padded = f"{day:02}"
-
-    generate(day_padded)
+    generate(day=f"{int(sys.argv[1]):02}", title=sys.argv[2])
 
 
 if __name__ == "__main__":
