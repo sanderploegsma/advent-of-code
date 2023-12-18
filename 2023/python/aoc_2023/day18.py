18bf161e4cb462add0c645d925fc46166ee9ac08
Sander Ploegsma <sanderploegsma@gmail.com>
2023-12-18 11:54:49 +0100

    2023 day 18 refactor

--- 2023/python/aoc_2023/day18.py
+++ 2023/python/aoc_2023/day18.py
@@ -9,21 +9,21 @@ from typing import TextIO
 from aoc_2023.grid import NORTH, EAST, SOUTH, WEST
 
 
-def calculate_area(directions: list[tuple[complex, int]]):
+def calculate_area(directions: list[complex]):
     """
     Calculate the area covered by digging a trench following the given directions.
 
-    :param directions: List of (direction, steps).
-    :return: The area in cubic meters.
+    :param directions: List of directions to dig.
+    :return: The area of the trench in cubic meters.
     """
     trench = [0]
-    for direction, steps in directions:
-        for _ in range(steps):
-            trench.append(trench[-1] + direction)
+    for direction in directions:
+        trench.append(trench[-1] + direction)
 
+    area_edges = sum(abs(d.real + d.imag) for d in directions)
     shoelace_area = abs(sum(p1.real * p2.imag - p2.real * p1.imag for p1, p2 in pairwise(trench))) // 2
-    area_inside = shoelace_area - len(trench) // 2 + 1
-    return int(area_inside + len(trench) - 1)  # Subtract 1 because the starting point occurs at [0] and [-1]
+    area_interior = shoelace_area - area_edges // 2 + 1
+    return int(area_interior + area_edges)
 
 
 def part_one(file: TextIO) -> int:
@@ -31,11 +31,11 @@ def part_one(file: TextIO) -> int:
     Solve part one of the puzzle.
     """
 
-    def parse(line: str) -> tuple[complex, int]:
+    def parse(line: str) -> complex:
         directions = {"U": NORTH, "D": SOUTH, "L": WEST, "R": EAST}
 
         direction, steps, _ = line.split()
-        return directions[direction], int(steps)
+        return directions[direction] * int(steps)
 
     return calculate_area(list(map(parse, file)))
 
@@ -45,12 +45,12 @@ def part_two(file: TextIO) -> int:
     Solve part two of the puzzle.
     """
 
-    def parse(line: str) -> tuple[complex, int]:
+    def parse(line: str) -> complex:
         directions = {"0": EAST, "1": SOUTH, "2": WEST, "3": NORTH}
 
         line = line.strip()
         steps, direction = line[-7:-2], line[-2]
-        return directions[direction], int("0x" + steps, base=16)
+        return directions[direction] * int("0x" + steps, base=16)
 
     return calculate_area(list(map(parse, file)))
 
