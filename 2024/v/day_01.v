71f96f9fe781ae33166b342ef300dfd460977fee
Sander Ploegsma <sanderploegsma@gmail.com>
2024-11-22 23:06:10 +0100

    Run `v fmt`

--- 2024/v/day_01.v
+++ 2024/v/day_01.v
@@ -2,25 +2,21 @@ module day_01
 
 import os
 
-const input_path = "../input/01.txt"
+const input_path = '../input/01.txt'
 
 @[params]
 struct Config {
-    input ?string
+	input ?string
 }
 
 fn part_one(config Config) !int {
-    input := config.input or {
-        os.read_file(input_path)!
-    }
+	input := config.input or { os.read_file(input_path)! }
 
 	return input.len
 }
 
 fn part_two(config Config) !int {
-	input := config.input or {
-        os.read_file(input_path)!
-    }
-    
+	input := config.input or { os.read_file(input_path)! }
+
 	return input.len
 }
--- 2024/v/day_01_test.v
+++ 2024/v/day_01_test.v
@@ -3,7 +3,7 @@ module day_01
 import os
 
 fn test_part_one() ! {
-	assert part_one(input: "Hello, World!")! == 13
+	assert part_one(input: 'Hello, World!')! == 13
 }
 
 fn test_part_two() {
