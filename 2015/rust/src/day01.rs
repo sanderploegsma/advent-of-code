0f137ae15143d93dc93d224b27a885489186c2ae
Sander Ploegsma <sanderploegsma@gmail.com>
2025-09-24 19:45:16 +0200

    2015/01 in Rust -- tests

--- 2015/rust/src/day01.rs
+++ 2015/rust/src/day01.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
-fn main() {
-    let input = fs::read_to_string("input/day01.txt").expect("Failed to read input file");
-    let part_one = input.chars().fold(0, |acc, c| match c {
+fn part_one(input: &str) -> i32 {
+    input.chars().fold(0, |acc, c| match c {
         '(' => acc + 1,
         ')' => acc - 1,
         _ => acc,
-    });
-    println!("Part one: {}", part_one);
+    })
+}
 
-    let part_two = input
+fn part_two(input: &str) -> usize {
+    input
         .chars()
         .scan(0, |floor, c| {
             *floor += match c {
@@ -19,6 +19,37 @@ fn main() {
             };
             if *floor == -1 { None } else { Some(*floor) }
         })
-        .count();
-    println!("Part two: {}", part_two);
+        .count()
+        + 1
+}
+
+fn main() {
+    let input = fs::read_to_string("input/day01.txt").expect("Failed to read input file");
+
+    println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one("(())"), 0);
+        assert_eq!(part_one("()()"), 0);
+        assert_eq!(part_one("((("), 3);
+        assert_eq!(part_one("(()(()("), 3);
+        assert_eq!(part_one("))((((("), 3);
+        assert_eq!(part_one("())"), -1);
+        assert_eq!(part_one("))("), -1);
+        assert_eq!(part_one(")))"), -3);
+        assert_eq!(part_one(")())())"), -3);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(")"), 1);
+        assert_eq!(part_two("()())"), 5);
+    }
 }
